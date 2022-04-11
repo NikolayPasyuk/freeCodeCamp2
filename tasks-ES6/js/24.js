354c7ea03e9dbeef4ba8a28af544244d7a84da5c
NikolayPasyuk <kolyan-1812@mail.ru>
2022-04-11 18:20:13 +0300

    Handle a Fulfilled Promise with then

---
+++ tasks-ES6/js/24.js
@@ -0,0 +1,8 @@
+const makeServerRequest = new Promise((resolve, reject) => {
+    // responseFromServer is set to true to represent a successful response from a server
+    let responseFromServer = true;
+
+    makeServerRequest.then(result => {
+        console.log(result)
+    });
+});
