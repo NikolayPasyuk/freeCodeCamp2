507cce1b92a95fa93f974a74359025bcc57479a4
NikolayPasyuk <kolyan-1812@mail.ru>
2024-06-07 17:51:12 +0300

    feat: added new line between row values in for...of loop

---
+++ JS/JS_Pyramid_Generator/step_21-40/step-40.js
@@ -0,0 +1,15 @@
+const count = 8;
+const character = "#";
+const rows = []
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(i)
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result)
