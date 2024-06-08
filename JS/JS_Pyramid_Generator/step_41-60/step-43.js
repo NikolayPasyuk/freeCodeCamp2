35189da77c20eded1f0094de6610d729252a8d42
NikolayPasyuk <kolyan-1812@mail.ru>
2024-06-08 18:19:09 +0300

    fix: added 1 to .repeat() to avoid empty string display

---
+++ JS/JS_Pyramid_Generator/step_41-60/step-43.js
@@ -0,0 +1,15 @@
+const count = 8;
+const character = "#";
+const rows = []
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(character.repeat(i + 1))
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result)
