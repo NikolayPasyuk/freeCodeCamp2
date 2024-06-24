c5783dabd8a5861b9d45c099c14135594f59ea25
NikolayPasyuk <kolyan-1812@mail.ru>
2024-06-24 11:51:58 +0300

    feat: update goldText and text elements in buyWeapon function

--- JS/JS_Role_Playing_Game/script.js
+++ JS/JS_Role_Playing_Game/script.js
@@ -90,6 +90,9 @@ function buyWeapon() {
   if (gold >= 30) {
     gold -= 30;
     currentWeaponIndex++;
+    goldText.innerText = gold;
+
+    text.innerText = "You now have a new weapon.";
   }
 }
 
