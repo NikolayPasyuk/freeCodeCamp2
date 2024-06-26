ca18631cd2ce66f6de729669f0f0d3a297381db9
NikolayPasyuk <kolyan-1812@mail.ru>
2024-06-26 20:58:45 +0300

    feat: added win condition for dragon in attack function

--- JS/JS_Role_Playing_Game/script.js
+++ JS/JS_Role_Playing_Game/script.js
@@ -168,10 +168,15 @@ function attack() {
   monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
   healthText.innerText = health;
   monsterHealthText.innerText = monsterHealth;
+
   if (health <= 0) {
     lose();
   } else if (monsterHealth <= 0) {
-    defeatMonster();
+    if (fighting === 2) {
+      winGame();
+    } else {
+      defeatMonster();
+    }
   }
 }
 
