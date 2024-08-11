92dfa13e3672546382c1bf84a76ba18f9d5b507a
NikolayPasyuk <kolyan-1812@mail.ru>
2024-08-11 13:27:04 +0300

    feat: declared getCaloriesFromInputs function with list parameter

--- JS/JS_Calorie_Counter/script.js
+++ JS/JS_Calorie_Counter/script.js
@@ -32,4 +32,6 @@ function addEntry() {
   targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
 }
 
+function getCaloriesFromInputs(list) {}
+
 addEntryButton.addEventListener('click', addEntry)
