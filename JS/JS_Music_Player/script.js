3feaf58c08c66b6f7d9ffd230ddceb4ade66253c
NikolayPasyuk <kolyan-1812@mail.ru>
2024-12-11 20:03:40 +0300

    feat: removed playing class from playButton and paused the song

--- JS/JS_Music_Player/script.js
+++ JS/JS_Music_Player/script.js
@@ -105,6 +105,8 @@ const playSong = (id) => {
 
 const pauseSong = () => {
   userData.songCurrentTime = audio.currentTime;
+  playButton.classList.remove('playing');
+  audio.pause();
 }
 
 const renderSongs = (array) => {
