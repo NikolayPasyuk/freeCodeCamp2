52c1c169a1bbd034c1aa812ad9f42e89f193c5da
NikolayPasyuk <kolyan-1812@mail.ru>
2024-12-13 18:31:52 +0300

    feat: removed "aria-current" attribute from all playlist song elements

--- JS/JS_Music_Player/script.js
+++ JS/JS_Music_Player/script.js
@@ -135,6 +135,7 @@ const highlightCurrentSong = () => {
   const songToHighlight = document.getElementById(`song-${userData?.currentSong?.id}`);
 
   playlistSongElements.forEach((songEl) => {
+    songEl.removeAttribute('aria-current');
   });
 };
 
