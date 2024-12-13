5198bd851c2355f97681b6dba3b2c8eba9a525b4
NikolayPasyuk <kolyan-1812@mail.ru>
2024-12-13 17:47:38 +0300

    feat: added check for null currentSong in playNextSong function

--- JS/JS_Music_Player/script.js
+++ JS/JS_Music_Player/script.js
@@ -110,7 +110,9 @@ const pauseSong = () => {
 }
 
 const playNextSong = () => {
-
+  if (userData?.currentSong === null) {
+    playSong(userData?.songs[0].id);
+  }
 };
 
 const renderSongs = (array) => {
