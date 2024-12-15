151515d08c7ea7bf0181904f0c87b34e447e2a27
NikolayPasyuk <kolyan-1812@mail.ru>
2024-12-15 17:00:04 +0300

    feat: added aria-label attribute update for play button based on the current or first song

--- JS/JS_Music_Player/script.js
+++ JS/JS_Music_Player/script.js
@@ -173,12 +173,14 @@ const renderSongs = (array) => {
   playlistSongs.innerHTML = songsHTML;
 };
 
-const setPlayButtonAccessibleText = () => {
+const setPlayButtonAccessibleText = (qualifiedName, value) => {
   const song = userData?.currentSong || userData?.songs[0];
+  playButton.setAttribute('aria-label', song?.title ?
+    `Play ${song.title}` : 'Play');
 };
 
 const getCurrentSongIndex = () => {
-  return userData?.songs.indexOf(userData?.currentSong)
+  return userData?.songs.indexOf(userData?.currentSong);
 };
 
 playButton.addEventListener('click', () => {
