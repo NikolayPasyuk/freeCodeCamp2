cff722644ce75ecad9c41e4eef77ef8b75c361a9
NikolayPasyuk <kolyan-1812@mail.ru>
2024-12-19 19:37:25 +0300

    feat: added event listener for 'ended' event on audio element

--- JS/JS_Music_Player/script.js
+++ JS/JS_Music_Player/script.js
@@ -244,6 +244,9 @@ previousButton.addEventListener('click', playPreviousSong);
 
 shuffleButton.addEventListener('click', shuffle);
 
+audio.addEventListener('ended', () => {
+});
+
 const sortSongs = () => {
   userData?.songs.sort((a, b) => {
     if (a.title < b.title) {
