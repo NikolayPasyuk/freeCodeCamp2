f445a51def99d86571ee3836e75a79790e65cef5
NikolayPasyuk <kolyan-1812@mail.ru>
2024-12-19 19:56:41 +0300

    refactor: simplified conditional check in playPreviousSong function

--- JS/JS_Music_Player/script.js
+++ JS/JS_Music_Player/script.js
@@ -125,9 +125,8 @@ const playNextSong = () => {
 };
 
 const playPreviousSong = () => {
-  if (userData?.currentSong === null) {
-    return
-  } else {
+  if (userData?.currentSong === null) return;
+  else {
     const currentSongIndex = getCurrentSongIndex();
     const previousSong = userData?.songs[currentSongIndex - 1];
     playSong(previousSong.id);
