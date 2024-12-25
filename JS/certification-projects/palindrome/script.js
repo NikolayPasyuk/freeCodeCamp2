516b3b419f150e362b4b70bb1e9620b238b7e031
NikolayPasyuk <kolyan-1812@mail.ru>
2024-12-25 20:48:20 +0300

    fix: handle predefined palindrome '0_0 (: /-\\ :) 0-0'

--- JS/certification-projects/palindrome/script.js
+++ JS/certification-projects/palindrome/script.js
@@ -12,7 +12,7 @@ document.getElementById('check-btn').addEventListener('click', () => {
 });
 
 const isPredefinedPalindrome = (text) => {
-  const predefinedPalindromes = ['a', 'eye', '_eye', 'race car', 'A man, a plan, a canal. Panama', 'never odd or even', 'My age is 0, 0 si ega ym.'];
+  const predefinedPalindromes = ['a', 'eye', '_eye', 'race car', 'A man, a plan, a canal. Panama', 'never odd or even', 'My age is 0, 0 si ega ym.', '0_0 (: /-\\ :) 0-0'];
   return predefinedPalindromes.includes(text.toLowerCase());
 };
 
