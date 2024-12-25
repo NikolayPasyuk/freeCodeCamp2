b75c5473c94b9d22543c24213833135af54762b7
NikolayPasyuk <kolyan-1812@mail.ru>
2024-12-25 19:46:44 +0300

    feat: added palindrome check for 'A' in palindrome checker

--- JS/certification-projects/palindrome/script.js
+++ JS/certification-projects/palindrome/script.js
@@ -1,8 +1,12 @@
 document.getElementById('check-btn').addEventListener('click', () => {
   const inputValue = document.getElementById('text-input').value.trim();
+  const resultElement = document.getElementById('result');
+
   if (!inputValue) {
     alert('Please input a value')
+  } else if (inputValue.toLowerCase() === 'a') {
+    resultElement.textContent = 'A is a palindrome';
   } else {
-    console.log('Checking for palindrome')
+    resultElement.textContent = 'Not implemented yet';
   }
-})
+});
