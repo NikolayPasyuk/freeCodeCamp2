7ce7206ca32447ea0b56fdcc23d3ee0dfed8e679
NikolayPasyuk <kolyan-1812@mail.ru>
2024-12-31 19:27:16 +0300

    feat: added split, reverse, and join methods to formattedDate

--- JS/JS_Data_Formatter/script.js
+++ JS/JS_Data_Formatter/script.js
@@ -14,6 +14,7 @@ currentDateParagraph.textContent = formattedDate;
 dateOptionsSelectElement.addEventListener('change', () => {
   switch (dateOptionsSelectElement.value) {
     case "yyyy-mm-dd":
-      currentDateParagraph.textContent = formattedDate;
+      currentDateParagraph.textContent = formattedDate
+        .split('-').reverse().join('-');
   }
 });
