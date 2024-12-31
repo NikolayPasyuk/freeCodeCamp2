24e59029e65f47ba4aa80d194a02b522dd4a1d71
NikolayPasyuk <kolyan-1812@mail.ru>
2024-12-31 19:31:21 +0300

    feat: added new case "mm-dd-yyyy-h-mm" with empty template literals

--- JS/JS_Data_Formatter/script.js
+++ JS/JS_Data_Formatter/script.js
@@ -17,5 +17,8 @@ dateOptionsSelectElement.addEventListener('change', () => {
       currentDateParagraph.textContent = formattedDate
         .split('-').reverse().join('-');
       break;
+    case 'mm-dd-yyyy-h-mm':
+      currentDateParagraph.textContent = ``;
+      break;
   }
 });
