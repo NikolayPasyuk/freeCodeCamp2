1bc6b8aae059eb12f815ea09c3498e77bd69fd78
NikolayPasyuk <kolyan-1812@mail.ru>
2025-01-04 13:16:06 +0300

    feat: assigned team variable to teamName.textContent

--- JS/JS_Football_Team_Cards/script.js
+++ JS/JS_Football_Team_Cards/script.js
@@ -34,4 +34,6 @@ const myFavoriteFootballTeam = {
 Object.freeze(myFavoriteFootballTeam);
 const {sport, team, year, players} = myFavoriteFootballTeam;
 const {coachName} = myFavoriteFootballTeam.headCoach;
+
 typeOfSport.textContent = sport;
+teamName.textContent = team;
