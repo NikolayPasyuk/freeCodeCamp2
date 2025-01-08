628659e7c7255acad9a55547ae2bfddf785b4d9c
NikolayPasyuk <kolyan-1812@mail.ru>
2025-01-08 21:35:48 +0300

    feat: added functionality to close confirm modal on Cancel button click

--- JS/JS_LocalStorage/script.js
+++ JS/JS_LocalStorage/script.js
@@ -15,4 +15,7 @@ let currentTask = {};
 
 openTaskFormBtn.addEventListener('click', () =>
   taskForm.classList.toggle('hidden'));
+
 closeTaskFormBtn.addEventListener('click', () => confirmCloseDialog.showModal());
+
+cancelBtn.addEventListener('click', () => confirmCloseDialog.close());
