fcbf04c5927cdc9ceb3ee989e8377e02b3f31852
NikolayPasyuk <kolyan-1812@mail.ru>
2025-01-08 21:41:19 +0300

    feat: added functionality to discard changes and close both modals

--- JS/JS_LocalStorage/script.js
+++ JS/JS_LocalStorage/script.js
@@ -19,3 +19,8 @@ openTaskFormBtn.addEventListener('click', () =>
 closeTaskFormBtn.addEventListener('click', () => confirmCloseDialog.showModal());
 
 cancelBtn.addEventListener('click', () => confirmCloseDialog.close());
+
+discardBtn.addEventListener('click', () => {
+  confirmCloseDialog.close();
+  taskForm.classList.toggle('hidden');
+});
