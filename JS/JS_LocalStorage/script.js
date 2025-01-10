0bec1989dba2180e0517599ff71b69fa49eaac28
NikolayPasyuk <kolyan-1812@mail.ru>
2025-01-10 19:38:49 +0300

    feat: created taskObj as an empty object and logged it to the console

--- JS/JS_LocalStorage/script.js
+++ JS/JS_LocalStorage/script.js
@@ -27,6 +27,9 @@ discardBtn.addEventListener('click', () => {
 
 taskForm.addEventListener('submit', (e) => {
   e.preventDefault()
-});
 
-const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+  const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+
+  const taskObj = {};
+  console.log(taskObj);
+});
